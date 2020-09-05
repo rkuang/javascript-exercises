08ac7bd57836130c50d6307c35ab006d0f9ad00d
Ricky Kuang <rkuang26@gmail.com>
2020-09-05 16:13:39 -0700

    complete fibonacci, findTheOldest, and getTheTitles

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 1) return 'OOPS'
+    if (a == 1 || a == 2) return 1;
+    return fibonacci(a-1) + fibonacci(a-2);
 }
 
 module.exports = fibonacci
--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    people.slice(1).forEach(person => {
+        if (ageOf(person) > ageOf(oldestPerson))
+            oldestPerson = person;
+    });
+    return oldestPerson;
+}
 
+function ageOf(person) {
+    if (person.yearOfDeath)
+        return person.yearOfDeath-person.yearOfBirth;
+    else
+        return new Date().getFullYear()-person.yearOfBirth;
 }
 
 module.exports = findTheOldest
